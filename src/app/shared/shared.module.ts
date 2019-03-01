b7a9204330716f1667b1d65c0639782bab45e26a
Nlb <nilbertrr@gmail.com>
2019-02-28 20:11:40 -0500

    -add material component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,57 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatCheckboxModule, MatRadioModule } from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+   
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatProgressBarModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule, 
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatProgressBarModule
+  ],
 })
 export class SharedModule { }
