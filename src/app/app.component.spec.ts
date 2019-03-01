72ba3c016171fcee750cd692571dfa1a2e7a10d4
Nlb <nilbertrr@gmail.com>
2019-02-28 19:59:36 -0500

    -add shared module and core module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'octocat-serch'`, () => {
+  it(`should have as title 'octocat-search'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('octocat-serch');
+    expect(app.title).toEqual('octocat-search');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to octocat-serch!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to octocat-search!');
   });
 });
